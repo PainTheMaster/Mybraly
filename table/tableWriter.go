caf69998d63e0ee21f667c1a8ac36971b6826ba9
KenOnLetsNote <feruos@gmail.com>
2020-03-10 07:01:16 +0900

    in the middle of table writer

--- table/tableWriter.go
+++ table/tableWriter.go
@@ -1,1 +1,19 @@
 package table
+
+import (
+	"bufio"
+	"os"
+)
+
+func writeTable(matrix [][]float64, file os.File) {
+
+	writer := bufio.NewWriter(&file)
+
+	rows := len(matrix)
+	for i := 0; i <= rows-1; i++ {
+		cols := len(matrix[i])
+		for j := 0; j <= cols-1; j++ {
+			writer.WriteString()
+		}
+	}
+}
