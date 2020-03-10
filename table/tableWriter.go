88cc9ecc9e1489c0d67dec2a854ae3d6910c4f49
KenOnLetsNote <feruos@gmail.com>
2020-03-10 21:10:26 +0900

    extra comma removed, test ok

--- table/tableWriter.go
+++ table/tableWriter.go
@@ -14,9 +14,13 @@ func WriteTable(matrix [][]float64, file *os.File) {
 	rows := len(matrix)
 	for i := 0; i <= rows-1; i++ {
 		cols := len(matrix[i])
-		for j := 0; j <= cols-1; j++ {
+		for j := 0; j <= cols-2; j++ {
 			writer.WriteString(strconv.FormatFloat(matrix[i][j], 'e', 10, 64))
 			writer.WriteByte(',')
+		}
+		{
+			j := cols - 1
+			writer.WriteString(strconv.FormatFloat(matrix[i][j], 'e', 10, 64))
 		}
 		writer.WriteString("\n")
 		writer.Flush()
